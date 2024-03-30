fd33d3b23a2c7f8a14b4a89563121e57ffe56920
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-03-31 04:20:29 +0530

    tailwind setup and all pages working properly

--- users/models.py
+++ users/models.py
@@ -13,10 +13,10 @@ class Profile(models.Model):
     user=models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo= models.ImageField(upload_to='users/%Y/%M/%d',blank=True)
     mobile= models.CharField(max_length=15)
-    dob = models.DateField()
+    dob = models.DateField(blank=True, null=True)
     gender= models.CharField(max_length=10,choices=GenderChoies.choices)
-    address= models.CharField(max_length=200)
-    pincode= models.IntegerField()
+    address= models.CharField(max_length=200,blank=True, null=True)
+    pincode= models.IntegerField(blank=True, null=True)
     age = models.IntegerField(default=18)
 
 
