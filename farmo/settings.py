99099bd7fa833445f951fd40d412924598fd12df
Vaibhav Nagargoje <92326795+vaibhavnagargoje@users.noreply.github.com>
2024-04-05 04:00:29 +0530

    Update settings.py

--- farmo/settings.py
+++ farmo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#1@xjy3vc^0@64+31v#a!p9%h7+)m$skh*f^pv)y&h$%8&)nvt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
